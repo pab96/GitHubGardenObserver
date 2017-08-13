558a3ffca12a37df8e61418bbccc99d556b4a8f2
pab96 <pab96@cam.ac.uk>
2017-08-13 22:58:00 +0100

    --date

---
+++ calculateTimerOnOffTimes.py
@@ -0,0 +1,18 @@
+R_A=100
+R_B=5000
+C=100e-3
+
+
+t_1=0.693*(R_A+R_B)*C
+t_2=0.693*R_B*C
+T=t_1+t_2
+
+print("t_1= {}".format(t_1))
+print("t_2= {}".format(t_2))
+print("T= {}".format(T))
+
+
+t_1=
+t_2=
+T=
+
